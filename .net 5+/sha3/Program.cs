61be89d0cb895b9789af1415f5820f2096af3342
7kruger ⠀ <ekruug3r@gmail.com>
2023-01-24 13:32:28 +0500

    initial

---
+++ .net 5+/sha3/Program.cs
@@ -0,0 +1,14 @@
+﻿using SHA3.Net;
+using System.Text;
+
+namespace sha3;
+
+public class Program
+{
+    public static void Main()
+    {
+        var hash = Sha3.Sha3256().ComputeHash(Encoding.UTF8.GetBytes("123"));
+        var res = Convert.ToHexString(hash).ToLower();
+        Console.WriteLine(res);
+    }
+}
---
+++ .net core 3.1/hash/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using SHA3.Net;
+using System.Security.Cryptography;
+
+namespace hash
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var hash = Sha3.Sha3256().ComputeHash(Encoding.UTF8.GetBytes("admin"));
+			var result = BitConverter.ToString(hash).Replace("-", "").ToLower();
+			Console.WriteLine(result);
+
+			using (var sha3 = Sha3.Sha3256())
+			{
+				var bytes = sha3.ComputeHash(Encoding.UTF8.GetBytes("admin"));
+				var res = BitConverter.ToString(bytes).Replace("-","").ToLower();
+
+				Console.WriteLine(res);
+			}
+		}
+	}
+}
